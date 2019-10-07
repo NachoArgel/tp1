b4187187f90f6fe26358a646a8ead95423d5eedb
felipegarciamorteo <56198889+felipegarciamorteo@users.noreply.github.com>
2019-10-07 13:06:42 -0300

    Update hash_abierto.c
    Modificacion lunes 13hs. Termine redimensionar(puede faltar algo), agregué un caso en hash guardar y algunas cosas mas.

--- hash_abierto.c
+++ hash_abierto.c
@@ -1,16 +1,20 @@
 #include "hash.h"
 #include "lista.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
+//DEFINE TAM 32
 const int primos[] = {41,101,211,401,809,1601,3209,6421,12809,25603,50021,100003,200003,
 400009,800011};
 
 
 
-typedef campo campo_t{
+typedef struct campo{
 	char* clave;
 	void* valor;
-};
+}campo_t;
 
-campo_t* campo_crear(clave,valor){
+campo_t* campo_crear(const char* clave,void* valor){
 	campo_t* campo = malloc(sizeof(campo_t));
 	if(campo==NULL){
 		return NULL;
@@ -20,14 +24,14 @@ campo_t* campo_crear(clave,valor){
 	return campo;
 }
 
-struct hash hash_t{
+struct hash{
 	lista_t** listas;
 	size_t capacidad; 
 	size_t cantidad;
 	void (*hash_destruir_dato_t)(void *);
 };
 
-struct hash_iter hash_iter_t{
+struct hash_iter{
 	hash_t* hash;
 	campo_t* actual;
 	campo_t* anterior;
@@ -40,17 +44,17 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
 	if(hash==NULL){
 		return NULL;
 	}
-	hash->listas = malloc(TAM*sizeof(lista_t*));	
+	hash->listas = malloc(primos[0]*sizeof(lista_t*));	
 	if(hash->listas = NULL){
 		free(hash);
 		return NULL;
 	}
 	hash->cantidad = 0;
-	hash->capacidad = TAM;}
-	hash->
+	hash->capacidad = primos[0];//hash->capacidad = TAM;
 }
 
 bool hash_guardar(hash_t *hash, const char *clave, void *dato){
+	
 	if(hash->cantidad >= 2*hash->capacidad){
 		if(!hash_redimensionar(hash)){
 			return false;
@@ -59,51 +63,78 @@ bool hash_guardar(hash_t *hash, const char *clave, void *dato){
 	int pos=hashing(clave,hash);
 	
 	campo_t* campo = campo_crear(clave,dato);
-	if (campo == NULL)return NULL;
+	if (campo == NULL){
+		return false;
+	}
 	if (hash->listas[pos] == NULL){
-		hash->listas[pos] = lista_crear());
-		if(hash->listas[pos] == NULL)return NULL;
+		hash->listas[pos] = lista_crear();
+		if(hash->listas[pos] == NULL){
+			return false;
+		}
 	}
-	if(!lista_insertar_ultimo(hash->listas[pos],campo)){
-		return NULL;
+	//Chequeo que no exista la clave que recibi, y de existir le actualizo el dato
+	lista_iter_t* iter = lista_iter_crear(hash->listas[pos]);
+	if(!iter){
+		free(campo);
+		return false;
 	}
+	//Creo un booleano para saber si ya inserte o no el valor
+	bool insertado = false;
+	while(!lista_iter_al_final(iter) && lista_iter_ver_actual(iter)->clave != clave){
+		if(lista_iter_ver_actual(iter)->clave == clave){
+			if(!lista_iter_insertar(iter,campo))return false;
+			insertado = true;
+		}
+			lista_iter_avanzar(iter);
+	}
+	if(!insertado){			
+		if(!lista_iter_insertar(iter,campo)){
+			return false;
+		}
+	}
+	lista_iter_destruir(iter);
 	hash->cantidad++;	
 	return true;
 }
 
 void *hash_borrar(hash_t *hash, const char *clave){
+	
 	if(hash->cantidad <= (hash->capacidad)/4){
 		if(!hash_redimensionar(hash)){
 			return NULL;
 		}
 	}
 	int pos = hashing(clave,hash);
-	if((lista_esta_vacia(hash->listas[pos]))){
+	if(lista_esta_vacia(hash->listas[pos])){
 		return NULL;
 	}
 	lista_iter_t* iter = lista_iter_crear(hash->listas[pos]);
-	while(!lista_iter_al_final(iter) && lista_iter_ver_actual(iter)->dato->clave != clave){
+	while(!lista_iter_al_final(iter) && lista_iter_ver_actual(iter)->clave != clave){
 		lista_iter_avanzar(iter);
 	}
-	if (lista_iter_al_final(iter))return NULL;
-	hash->cantidad --;
+	if (lista_iter_al_final(iter)){
+		return NULL;
+	}
+	hash->cantidad--;
 	return lista_iter_borrar(iter);
-	
 }
 
 void *hash_obtener(const hash_t *hash, const char *clave){
 	int pos = hashing(clave,hash);
 	
-	if((lista_esta_vacia(hash->listas[pos]))){
+	if(lista_esta_vacia(hash->listas[pos])){
 		return NULL;
 	}
 	lista_iter_t* iter = lista_iter_crear(hash->listas[pos]);
-	while(!lista_iter_al_final(iter) && lista_iter_ver_actual(iter)->dato->clave != clave){
+	while(!lista_iter_al_final(iter) && lista_iter_ver_actual(iter)->clave != clave){
 		lista_iter_avanzar(iter);
 	}
-	if (lista_iter_al_final(iter))return NULL;
+	if (lista_iter_al_final(iter)){
+		return NULL;
+		}
 	return lista_iter_ver_actual(iter);
 }		
+
 bool hash_pertenece(const hash_t *hash, const char *clave){
 	return (hash_obtener(hash,clave)!= NULL);
 }
@@ -116,19 +147,57 @@ void hash_destruir(hash_t *hash){
 
 }
 
-bool hash_redimensionar(hash){
+bool hash_redimensionar(hash_t* hash){
 	
 	if (hash->capacidad <= 800011){
-		sizeof dim = 0
-		for (i=0 ; i<15; i++){
-			if (primos[i]==capacidad;
+		size_t dim = 0;
+		for (int i=0 ; i<15; i++){
+			if (primos[i]==hash->capacidad);
 			dim = i+1;
 		}
-		lista_t** listas_nuevo = malloc(dim*sizeof(lista_t*))
+		lista_t** listas_nuevo = malloc(primos[dim]*sizeof(lista_t*));
+		if(!listas_nuevo)return false;
 	}else{
-		lista_t** listas_nuevo = malloc(2*sizeof(lista_t*))
+		lista_t** listas_nuevo = malloc(2*sizeof(lista_t*));
+		if(!listas_nuevo)return false;
 	}
-
+	//Copio todos los datos del hash viejo que ya no tiene lugar en listas_nuevo, 
+	//siempre verificando que si algo no se puede hacer se destruya y libere toda
+	//la memoria en uso
+	hash_iter_t* hash_iter = hash_iter_crear(hash);
+	if(!hash_iter){
+		free(listas_nuevo);
+		return false;
+	}
+	
+	size_t i = 0;
+	while(!hash_iter_al_final(hash_iter)){
+		if(hash->listas[i]!=NULL){
+			i++;
+		}else{
+			lista_iter_t* lista_iter = lista_iter_crear(hash->listas[i]);
+			if(!lista_iter){
+				free (listas_nuevo);
+				hash_iter_destruir(hash_iter);
+				return false;
+			}
+			while(!lista_iter_al_final(lista_iter)){
+				
+				campo_t* campo = lista_iter_borrar(lista_iter);
+				int pos = hashing(campo->clave,hash);
+				if(!lista_insertar_ultimo(listas_nuevo[pos],campo))return false;
+				lista_iter_avanzar(lista_iter);
+				hash_iter_avanzar(hash_iter);
+			
+			}
+			lista_destruir(hash->listas[i],hash->hash_destruir_dato_t);
+			lista_iter_destruir(lista_iter);
+			i++;
+		}
+	}
+	hash_iter_destruir(hash_iter);
+	hash->listas = listas_nuevo;
+	return true;
 }
 
 hash_iter_t *hash_iter_crear(const hash_t *hash);
